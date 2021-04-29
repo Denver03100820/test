0dc76de69847ea3d687ff2fde5281e606a305177
Chony-Or <chonyor76@gmail.com>
2021-04-29 21:34:44 +0800

    update

--- FINAL/HomePage.php
+++ FINAL/HomePage.php
@@ -251,34 +251,12 @@
 
                                
 
-                                <?php
-                                    $server = "localhost";
-                                    $username = "root";
-                                    $password = "";
-                                    $DB = "lhoyzki_ordering";
-
-                                    $connection = new mysqli($server,$username,$password,$DB); //connection
-                                  
-                                    if(isset($_POST['Submit']))
-                                    {
-
-                                        $customer_Feedback = $_POST["customer_Feedback"];
-
-                                        $sqlvar ="INSERT INTO feedback_tbl(customer_Feedback) VALUES
-                                        ('{$customer_Feedback}')";
-
-                                        
-		                               var_dump($connection->query($sqlvar));
-		                            
-                                       var_dump($sqlvar);
-                                    }
-
-                                ?>
-                                 <form action="HomePage.php" method="POST">
-                                    <textarea rows="7" cols="40" name="customer_Feedback"></textarea><br><br>
+                                 <form action="insertfeedback.php" method="POST">
+                                    <input textarea rows="7" cols="40" name="customer_Feedback"></textarea><br><br>
                                     <input type="Submit" name="Submit">
                                 
                                 </form>
+
                             </li>
                         </ul>
                     </div>
