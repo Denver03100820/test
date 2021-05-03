64d8b6572b928b6143d7e7018cad57fa56a1be20
Chony-Or <chonyor76@gmail.com>
2021-05-03 10:58:49 +0800

    update

--- FINAL/HomePage.php
+++ FINAL/HomePage.php
@@ -4,48 +4,11 @@ include 'header.php';
 
 <body class="bodycolor">
 
-    <!--Hero Section-->
-    <!--<section id="hero">
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-
-            <!--Indicators-->
-            <!--<ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-                <li data-target="#myCarousel" data-slide-to="3"></li>
-            </ol>
-
-            <!--Wrapper for Slides-->
-           <!-- <div class="carousel-inner" role="listbox">
-                <div class="carousel-item active" data-interval="7000">
-                    <img class="d-md-block img-fluid" src="img/banner4.png">
-                </div>
-                <div class="carousel-item" data-interval="5000">-->
+ 
+                <div class="container" style="margin-top: 75px" >
                     <img class="d-md-block img-fluid" src="img/Mainpic.png">
-                <!--</div>
-                <div class="carousel-item" data-interval="5000">
-                    <img class="d-md-block img-fluid" src="img/banner1.gif">
                 </div>
-                <div class="carousel-item" data-interval="5000">
-                    <img class="d-md-block img-fluid" src="img/banner2.gif">
-                </div>
-            </div>
 
-            <!--Left and right-->
-           <!-- <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-
-        </div>
-    </section>
-    </section>
-    <!--End Hero Section-->
 
     <!--Favorites Section-->
     <section id="new">
