a7fe6434aecb30c50154be82b1588065cbd5e53f
arakimochi <kellyviryoso@gmail.com>
2021-05-06 00:11:26 +0800

    commit2

--- FINAL/milkshake.php
+++ FINAL/milkshake.php
@@ -11,7 +11,7 @@ include 'header.php';
     <!--Menu Section-->
     <div class="jumbotron">
         <br>
-        <h1 class="milkshake" style="font-family: 'Fantasy'; color: #725527; margin-top: 100px; margin-left: 15%;">MILKSHAKE</h1><br>
+        <h1 class="milkshake" style="font-family: 'Fantasy'; color: #725527; margin-top: 80px; margin-left: 15%;">MILKSHAKE</h1><br>
         <center><hr style="width: 71%; border-top: 1px solid black;">
     </div>
 
--- FINAL/milktea.php
+++ FINAL/milktea.php
@@ -9,126 +9,126 @@ include 'header.php';
 <body class="bodycolor">
 
   <!--Menu Section-->
-
+  <div class="jumbotron">
     <br>
-    <h1 class="milkshake" style="font-family: 'Fantasy'; color: #725527; margin-top: 100px; margin-left: 230px;">MILKTEA</h1><br>
-    <hr style="width: 71%; border-top: 1px solid; margin-left: 220px;">
+    <h1 class="milkshake" style="font-family: 'Fantasy'; color: #725527; margin-top: 80px; margin-left: 15%;">MILKTEA</h1><br>
+    <center><hr style="width: 71%; border-top: 1px solid black;">
+  </div>
       
-    <br><br>
-    <div class="container">
-    
-      <div class="row">
-       
-        <div class="col-md-3">
-          <div class="card">
-            <div class="card-body">
-              <a href="milktTaro.php">
-                <img src="img/ClassicTaroMT.png" class="centerimage">
-              </a>
-              <br><br>
-              <div class="textcolor"><center><h4><b>Classic Taro Milktea</b></h4></center><br>
-              <center><h5>₱100.00</h5></center></div>
-            </div>
-          </div>
-        </div>
-      
-        <div class="col-md-3">
-          <div class="card">
-            <div class="card-body">
-              <a href="milktOkinawa.php">
-                <img src="img/ClassicOkinawaMT.png" class="centerimage">
-              </a>
-              <br><br>
-              <div class="textcolor"><center><h4><b>Classic Okinawa Milktea</b></h4></center><br>
-              <center><h5>₱100.00</h5></center></div>
-            </div>
-          </div>
-        </div>
-      
-        <div class="col-md-3">
-          <div class="card">
-            <div class="card-body">
-              <a href="milktHokkaido.php">
-                <img src="img/HokkaidoMT.png" class="centerimage">
-              </a>
-              <br><br>
-              <div class="textcolor"><center><h4><b>Hokkaido Milktea</b></h4></center><br>
-              <center><h5>₱100.00</h5></center></div>
-            </div>
-          </div>
-        </div>
-      
-        <div class="col-md-3">
-          <div class="card">
-            <div class="card-body">
-              <a href="milktChoco.php">
-                <img src="img/ChocolateMT.png" class="centerimage">
-              </a>
-              <br><br>
-              <div class="textcolor"><center><h4><b>Chocolate Milktea</b></h4></center><br>
-              <center><h5>₱100.00</h5></center></div>
-            </div>
-          </div>
-        </div>
-      
-        <div class="col-md-3">
-          <br><br><br>
-          <div class="card">
-            <div class="card-body">
-              <a href="milktRedVelvet.php">
-                <img src="img/RedVelvetMT.png" class="centerimage">
-              </a>
-              <br><br>
-              <div class="textcolor"><center><h4><b>Red Velvet Milktea</b></h4></center><br>
-              <center><h5>₱100.00</h5></center></div>
-            </div>
-          </div>
-        </div>
-      
-        <div class="col-md-3">
-          <br><br><br>
-          <div class="card">
-            <div class="card-body">
-              <a href="milktWintermelon.php">
-                <img src="img/ClassicWintermelonMT.png" class="centerimage">
-              </a>
-              <br><br>
-              <div class="textcolor"><center><h4><b>Classic Wintermelon Milktea</b></h4></center><br>
-              <center><h5>₱100.00</h5></center></div>
-            </div>
-          </div>
-        </div>
+  <br><br>
+  <div class="container">
   
-        <div class="col-md-3">
-          <br><br><br>
-          <div class="card">
-            <div class="card-body">
-              <a href="milktCaramel.php">
-                <img src="img/PremiumCaramelSugarMT.png" class="centerimage">
-              </a>
-              <br>
-              <div class="textcolor"><center><h4><b>Premium Caramel Sugar Milktea</b></h4></center><br>
-              <center><h5>₱100.00</h5></center></div>
-            </div>
-          </div>
-        </div>
+    <div class="row">
       
-        <div class="col-md-3">
-          <br><br><br>
-          <div class="card">
-            <div class="card-body">
-              <a href="milktCNC.php">
-                <img src="img/CookiesNCream.png" class="centerimage">
-              </a>
-              <br><br>
-              <div class="textcolor"><center><h4><b>Cookies N Cream Milktea</b></h4></center><br>
-              <center><h5>₱100.00</h5></center></div>
-            </div>
+      <div class="col-md-3">
+        <div class="card">
+          <div class="card-body">
+            <a href="milktTaro.php">
+              <img src="img/ClassicTaroMT.png" class="centerimage">
+            </a>
+            <br><br>
+            <div class="textcolor"><center><h4><b>Classic Taro Milktea</b></h4></center><br>
+            <center><h5>₱100.00</h5></center></div>
           </div>
         </div>
       </div>
-    </div>  
-  </div> 
+    
+      <div class="col-md-3">
+        <div class="card">
+          <div class="card-body">
+            <a href="milktOkinawa.php">
+              <img src="img/ClassicOkinawaMT.png" class="centerimage">
+            </a>
+            <br><br>
+            <div class="textcolor"><center><h4><b>Classic Okinawa Milktea</b></h4></center><br>
+            <center><h5>₱100.00</h5></center></div>
+          </div>
+        </div>
+      </div>
+    
+      <div class="col-md-3">
+        <div class="card">
+          <div class="card-body">
+            <a href="milktHokkaido.php">
+              <img src="img/HokkaidoMT.png" class="centerimage">
+            </a>
+            <br><br>
+            <div class="textcolor"><center><h4><b>Hokkaido Milktea</b></h4></center><br>
+            <center><h5>₱100.00</h5></center></div>
+          </div>
+        </div>
+      </div>
+    
+      <div class="col-md-3">
+        <div class="card">
+          <div class="card-body">
+            <a href="milktChoco.php">
+              <img src="img/ChocolateMT.png" class="centerimage">
+            </a>
+            <br><br>
+            <div class="textcolor"><center><h4><b>Chocolate Milktea</b></h4></center><br>
+            <center><h5>₱100.00</h5></center></div>
+          </div>
+        </div>
+      </div>
+    
+      <div class="col-md-3">
+        <br><br><br>
+        <div class="card">
+          <div class="card-body">
+            <a href="milktRedVelvet.php">
+              <img src="img/RedVelvetMT.png" class="centerimage">
+            </a>
+            <br><br>
+            <div class="textcolor"><center><h4><b>Red Velvet Milktea</b></h4></center><br>
+            <center><h5>₱100.00</h5></center></div>
+          </div>
+        </div>
+      </div>
+    
+      <div class="col-md-3">
+        <br><br><br>
+        <div class="card">
+          <div class="card-body">
+            <a href="milktWintermelon.php">
+              <img src="img/ClassicWintermelonMT.png" class="centerimage">
+            </a>
+            <br><br>
+            <div class="textcolor"><center><h4><b>Classic Wintermelon Milktea</b></h4></center><br>
+            <center><h5>₱100.00</h5></center></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <br><br><br>
+        <div class="card">
+          <div class="card-body">
+            <a href="milktCaramel.php">
+              <img src="img/PremiumCaramelSugarMT.png" class="centerimage">
+            </a>
+            <br>
+            <div class="textcolor"><center><h4><b>Premium Caramel Sugar Milktea</b></h4></center><br>
+            <center><h5>₱100.00</h5></center></div>
+          </div>
+        </div>
+      </div>
+    
+      <div class="col-md-3">
+        <br><br><br>
+        <div class="card">
+          <div class="card-body">
+            <a href="milktCNC.php">
+              <img src="img/CookiesNCream.png" class="centerimage">
+            </a>
+            <br><br>
+            <div class="textcolor"><center><h4><b>Cookies N Cream Milktea</b></h4></center><br>
+            <center><h5>₱100.00</h5></center></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>   
   <br><br><br><br><br>  
   <!--End Menu Section-->
 
